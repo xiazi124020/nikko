6e59db429a3c349b480f848153eae98057c84156
xiazi124020 <77074100@qq.com>
2019-07-20 23:22:54 +0900

    LoginCheck

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Model\Employee;
+use Illuminate\Support\Facades\DB;
+
+class CustomerController extends CommonController
+{
+    //
+}
---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Model\Employee;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeController extends CommonController
+{
+    public function list() {
+        $loginPromission = session('employee')[0]['permission'];
+        $employees = Employee::where('permission', '>=', $loginPromission)->get();
+
+        // $employees = DB::table('employee')
+        //     ->join('contacts', 'users.id', '=', 'contacts.user_id')
+        //     ->join('orders', 'users.id', '=', 'orders.user_id')
+        //     ->select('users.*', 'contacts.phone', 'orders.price')
+        //     ->get();
+
+
+        return view('pages.employeelist', ['employees' => $employees]);
+    }
+}
