a28508034afba9711bd9012a543390db05c14734
xiazi124020 <77074100@qq.com>
2019-07-21 09:06:50 +0900

    common.blade.php

--- resources/views/pages/common.blade.php
+++ resources/views/pages/common.blade.php
@@ -158,8 +158,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/index" class="brand-link">
-      <img src="{{asset('resources/views/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
+      <img src="{{asset('resources/views/dist/img/nk.jpg')}}" alt="nikko Logo" class="brand-image img-circle elevation-3"
+           style="opacity: 1">
       <span class="brand-text font-weight-light">nikko</span>
     </a>
 
